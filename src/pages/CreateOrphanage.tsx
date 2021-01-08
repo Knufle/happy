2ffe5bdbabcda0daabf23d1ba40ddceb933aac30
Knufle <iamknufle@gmail.com>
2021-01-07 21:54:24 -0300

    finishing happy frontend

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -3,7 +3,6 @@ import { Map, Marker, TileLayer } from "react-leaflet";
 import { LeafletMouseEvent } from "leaflet";
 
 import { FiPlus } from "react-icons/fi";
-
 import "../styles/pages/create-orphanage.css";
 import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
